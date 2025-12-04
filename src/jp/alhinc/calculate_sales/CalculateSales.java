793c37634e7a3175655633372f030c323c0ca987
miura_kosuke <miura.kosuke@alhinc.jp>
2025-12-04 11:21:24 +0900

    エラー処理の追加が完了しました。 恐れ入りますが、ご確認よろしくお願いいたします。

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,13 +24,21 @@ public class CalculateSales {
 	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
 	private static final String FILE_NOT_EXIST = "支店定義ファイルが存在しません";
 	private static final String FILE_INVALID_FORMAT = "支店定義ファイルのフォーマットが不正です";
-
+	private static final String FILE_NOT_CONSECUTIVE = "売上ファイル名が連番になっていません";
+	private static final String TOTAL_AMOUNT_EXCEEDED_FIGURES = "合計⾦額が10桁を超えました";
 	/**
 	 * メインメソッド
 	 *
 	 * @param コマンドライン引数
 	 */
 	public static void main(String[] args) {
+		//エラー処理3-1
+		//コマンドライン引数が渡されているか
+		if (args.length != 1) {
+			System.out.println(UNKNOWN_ERROR);
+			return;
+		}
+
 		// 支店コードと支店名を保持するMap
 		Map<String, String> branchNames = new HashMap<>();
 		// 支店コードと売上金額を保持するMap
@@ -52,20 +61,41 @@ public class CalculateSales {
         //filesの数だけ繰り返すことで、
         //指定したパスに存在する全てのファイルの数だけ繰り返されます。
         for(int i = 0; i < files.length; i++) {
-        	  //ファイル名の取得とmatchesを使用してファイル名が「数字8桁.rcd」か判定
-        	  if(files[i].getName().matches("\\d{8}[.]rcd")) {
-        		  //売上ファイルの条件に当てはまったものだけ、List(ArrayList)に追加
-        		  rcdFiles.add(files[i]);
-        	  }
+			//ファイル名の取得とmatchesを使用してファイル名が「数字8桁.rcd」か判定
+			if(files[i].getName().matches("\\d{8}[.]rcd")) {
+				//売上ファイルの条件に当てはまったものだけ、List(ArrayList)に追加
+				rcdFiles.add(files[i]);
+			}
+		}
+        //エラー処理3-3
+    	//ファイルかディレクトリなのかを確認
+		//ファイル名の取得とmatchesを使用してファイル名が「数字8桁.rcd」か判定
+        for(int i = 0; i < rcdFiles.size(); i++) {
+        	if(rcdFiles.get(i).isFile() && files[i].getName().matches("\\d{8}[.]rcd")) {
+        		continue;
+    		} else {
+    			System.out.println(UNKNOWN_ERROR);
+    			return;
+    		}
+
         }
 
+
         //エラー処理2-1
+        //売上ファイルを保持しているListをソートする
+        Collections.sort(rcdFiles);
+
+        //売上ファイルの連番を確認
         for(int i = 0; i < rcdFiles.size() -1; i++) {
 
-        	int former = Integer.parseInt(rcdFiles.get(i).substring(0, 8));
-        	int latter = Integer.parseInt(次のファイル名.substring(0, 8));
+        	//売上ファイル名の先頭の数字8文字を切り出し、int型に変換
+        	int former = Integer.parseInt(rcdFiles.get(i).getName().substring(0, 8));
+        	int latter = Integer.parseInt(rcdFiles.get(i + 1).getName().substring(0, 8));
 
+        	//2つのファイル名の数字を比較
         	if((latter - former) != 1) {
+        		System.out.println(FILE_NOT_CONSECUTIVE);
+        		return;
         	}
     	}
 
@@ -92,11 +122,39 @@ public class CalculateSales {
 					fileContents.add(line);
 				}
 
+				//エラー処理2-3
+				//支店定義ファイルに売上ファイルの支店コードが存在するか確認
+				if(!branchNames.containsKey(fileContents.get(0))) {
+					System.out.println(rcdFiles.get(i).getName() + "の支店コードが不正です");
+					return;
+				}
+
+				//エラー処理2-4
+				//売上ファイルの行数が2行ではなかった場合はエラー表示する
+				if(fileContents.size() != 2) {
+					System.out.println(rcdFiles.get(i).getName()+ "のフォーマットが不正です");
+					return;
+				}
+
+				//エラー処理3-2
+				//売上金額が数字なのか確認
+				if(!fileContents.get(1).matches("^[0-9]*$")) {
+					System.out.println(UNKNOWN_ERROR);
+	    			return;
+				}
+
 				//売上ファイルから読み込んだ売上金額をMapに加算していくために型の変換を行う
 				long fileSale = Long.parseLong(fileContents.get(1));
 
 				//読み込んだ売上金額を加算します
 				Long saleAmount = branchSales.get(fileContents.get(0)) + fileSale;
+
+				//エラー処理2-2
+				//売上⾦額の合計が10桁を超えたか確認する
+				if(saleAmount >= 10000000000L){
+					System.out.println(TOTAL_AMOUNT_EXCEEDED_FIGURES);
+					return;
+				}
 
 				//加算した売上金額をMapに追加します
 				branchSales.put(fileContents.get(0), saleAmount);
@@ -140,7 +198,7 @@ public class CalculateSales {
 		try {
 			File file = new File(path, fileName);
 			//エラー処理1-1
-			//ファイルの存在チェック
+			//支店定義ファイルの存在チェック
 			if(!file.exists()) {
 				System.out.println(FILE_NOT_EXIST);
 				return false;
